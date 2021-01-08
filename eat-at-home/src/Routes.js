4bf79baa807a1d48186ab845bfd2d7dde110d849
Heather Luttrell <possumdiva@heatherluttrell.com>
2021-01-07 21:49:48 -0500

    routes.js created and routes created

---
+++ eat-at-home/src/Routes.js
@@ -0,0 +1,36 @@
+// import react, { Component } from "react";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import "./style.css";
+
+
+// class App extends Component {
+
+//   render() {
+//     return (
+//       <div className="nav"> 
+
+//       <Router>
+//         <Switch>
+//           <Route exact path="/" component={Home} />
+//           <Route path="/profile" component={Profile} />
+//           <Route path="/about" component={About} />
+//           <Route path="/food" component={Food} />
+//           <Route path="/ratings" component={Ratings} />
+//           <Route path="reviews" component={Reviews} />
+//           <Route path="favorites" component={Favorties} />
+//         </Switch>
+//       </Router>
+
+
+
+
+
+
+
+
+//       </div>
+//     )
+//   }
+// }
+
+// export default App;
