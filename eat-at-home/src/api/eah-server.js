4a2f33d2461e02f77aaa85541c00ebb660ab30a7
ejw773 <ejw773@gmail.com>
2021-01-22 19:48:59 -0500

    changes api url to cors-anywhere

--- eat-at-home/src/api/eah-server.js
+++ eat-at-home/src/api/eah-server.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://cors-anywhere.herokuapp.com/https://eat-at-home-server.herokuapp.com/",
 });
 
 export default instance;
