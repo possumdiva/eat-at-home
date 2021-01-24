663285f5a658b2cb3a2717e4e1de981c51d8ae31
ejw773 <ejw773@gmail.com>
2021-01-23 20:18:15 -0500

    merges with development

--- eat-at-home/src/api/eah-server.js
+++ eat-at-home/src/api/eah-server.js
@@ -1,11 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-<<<<<<< HEAD
   baseURL: "http://localhost:3000",
-=======
-  baseURL: "https://cors-anywhere.herokuapp.com/https://eat-at-home-server.herokuapp.com/",
->>>>>>> development
 });
 
 export default instance;
