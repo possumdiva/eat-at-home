7cfe1089524ba3e93a0fda5118965b88a9c8cb0a
Stacey Graham <stacey.graham1106@gmail.com>
2021-01-24 02:59:42 -0500

    creating LogOut component for Google OAuth and refreshToken function to handle new tokenIDs

---
+++ eat-at-home/src/components/LogOut.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useGoogleLogout } from 'react-google-login';
+
+const clientId = 
+'696620567557-45c6kd5krdk8nai0oavp12aa0mt4rig2.apps.googleusercontent.com';
+
+function Logout() {
+    const onLogoutSuccess = (res) => {
+        console.log('Logged out Success');
+        alert('Logged out Successfully ✌🏾');
+    };
+
+    const onFailure = () => {
+        console.log('Handle failure cases');
+    };
+
+    const { signOut } = useGoogleLogout({
+        clientId,
+        onLogoutSuccess,
+        onFailure,
+    });
+
+    return (
+        <button onClick={signOut} className="button">
+            <img src="icons/google.svg" alt="google login" className="icon"></img>
+
+            <span className="buttonText">Sign out</span>
+        </button>
+    );
+}
+
+export default Logout;
