0a8c480a722edc6d872bf3ddd5491412a367d645
Stacey Graham <stacey.graham1106@gmail.com>
2021-01-26 12:13:58 -0500

    Revert "adding readme.md"
    This reverts commit 93b887ea4a5e4987c4c80e37726113b473e9e8c4.

--- .history/eat-at-home/src/api/eah-server_20210123230840.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-  baseURL: "https://cors-anywhere.herokuapp.com/https://eat-at-home-server.herokuapp.com/",
-});
-
-export default instance;
--- .history/eat-at-home/src/components/Rating_20210109122644.jsx
+++
@@ -1,5 +0,0 @@
-import React, { useState } from "react";
-import { IoIosStar, IoIosStarOutline } from 'react-icons/io'
-
-import React, { useState } from "react";
-import { IoIosStar, IoIosStarOutline } from 'react-icons/io'
