b5211b28a2ee775cecb0214276f18342001efd11
Heather Luttrell <possumdiva@heatherluttrell.com>
2021-01-25 20:01:45 -0500

    styling and reviews added to profile page

--- eat-at-home/src/components/About.js
+++ eat-at-home/src/components/About.js
@@ -4,37 +4,43 @@ import { NavBar } from "./NavBar";
 import image1 from "./images/Team/Wilcott.jpeg";
 import image2 from "./images/Team/Luttrell.jpeg";
 import image3 from "./images/Team/Graham.jpeg";
+import image from "./images/Team/aboutHeader.jpeg";
 
 function About() {
   return (
     <div className="main">
       <NavBar />
+      <div className="aboutHI">
+        <img className="aboutHI-image" src={image}></img>
+      </div>
       <div className="about">
         <div>
           <img className="about-image" src={image1}></img>
+          <div className="about-text">
+            <p>
+              Elijah is a full-stack developer living in South Carolina. He has
+              also worked as a photographer, prepress operator, and Chinese
+              teacher. He loves good food and great films.
+              https://elijahwilcott.com/
+            </p>
+          </div>
         </div>
-        <div className="info-text">
-          <p>
-            Elijah is a full-stack developer living in South Carolina. He has
-            also worked as a photographer, prepress operator, and Chinese
-            teacher. He loves good food and great films.
-            https://elijahwilcott.com/
-          </p>
-        </div>
+
         <div>
           <img className="about-image" src={image2}></img>
+          <div className="about-text">
+            <p>
+              Heather does web-developing with an eye for the front-end.
+              Full-stack by day, plant/animal lover, musician and artist by
+              night. She enjoys hiking, yoga, knitting and tormenting her
+              husband with healthy food and scrabble. www.heatherluttrell.com
+            </p>
+          </div>
         </div>
-        <div className="info-text">
-          <p>
-            Heather does web-developing with an eye for the front-end.
-            Full-stack by day, plant/animal lover, musician and artist by night.
-            She enjoys hiking, yoga, knitting and tormenting her husband with
-            healthy food and scrabble. www.heatherluttrell.com
-          </p>
-        </div>
+
         <div>
           <img className="about-image" src={image3}></img>
-          <div className="info-text">
+          <div className="about-text">
             <p>
               Stacey Graham is a Software Engineer who lives in Atlanta, GA with
               her family. She loves “ALL THINGS” Tech and also loves making
